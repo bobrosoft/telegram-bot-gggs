ff23e9856a8a0a827afe501a6a6d35b922476c75
bobrosoft <bobrosoft@yandex.ru>
2021-12-28 14:29:27 +0400

    CARD-1: App: use sample code to test bot integration

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('test');
+import dotenv from 'dotenv';
+import {Telegraf} from 'telegraf';
+
+dotenv.config();
+
+const bot = new Telegraf(String(process.env.BOT_TOKEN));
+bot.start(ctx => ctx.reply('Welcome'));
+bot.help(ctx => ctx.reply('Send me a sticker'));
+bot.on('sticker', ctx => ctx.reply('👍'));
+bot.hears('hi', ctx => ctx.reply('Hey there'));
+bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
