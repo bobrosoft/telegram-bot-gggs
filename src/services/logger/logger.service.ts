fd394b1d1ec624bd7862e261fe10b543473fb1d2
bobrosoft <bobrosoft@yandex.ru>
2022-01-16 20:59:18 +0400

    CARD-6: App: add Logger service (fixes)

--- src/services/logger/logger.service.ts
+++ src/services/logger/logger.service.ts
@@ -1,5 +1,10 @@
 export class LoggerService {
   log(service: string, message: string) {
-    console.log(`${new Date().toISOString().replace(/\.\d+Z$/, '')} [${service}] ${message}`);
+    console.log(
+      `${new Date()
+        //
+        .toLocaleString('ru-RU', {timeZone: 'Europe/Samara'})
+        .replace(/,\s/g, ' ')} [${service}] ${message}`,
+    );
   }
 }
