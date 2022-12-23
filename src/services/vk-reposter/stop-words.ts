eace68f9fa7d51f52b6896855ffed650b88c612a
bobrosoft <bobrosoft@yandex.ru>
2022-12-23 12:43:28 +0400

    CARD-11: VkReposterService: update stop-words

--- src/services/vk-reposter/stop-words.ts
+++ src/services/vk-reposter/stop-words.ts
@@ -9,6 +9,8 @@ export const commonStopWords: Array<string | RegExp> = [
   /сдам.*гараж/,
   /долгосрочную.*аренду/,
   /z[aа]/,
+  /clck/,
+  /диагноз/,
 ];
 
 export const groupStopWords: Array<string | RegExp> = [
