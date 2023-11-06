cbae3cb637a71b2ab0339eb219529b945af2f794
bobrosoft <bobrosoft@yandex.ru>
2023-11-06 19:15:23 +0400

    CARD-10: DevOps: add handy update scripts (fixes)

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,5 +5,6 @@ set -e
 SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_PATH
 
+runuser -u www -- git restore package-lock.json
 runuser -u www -- git pull
 systemctl restart telegram-bot-gggs
